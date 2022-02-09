a518d0e798c4efdbd7e6d3c4f572df3d619e799c
iAmRarted <aritz.velasco.pancorbo@gmail.com>
2022-02-09 09:21:31 +0100

    Matriz 1 formateado

--- Tema 6 - Tanda 2/src/ejercicio3/Matriz1.java
+++ Tema 6 - Tanda 2/src/ejercicio3/Matriz1.java
@@ -1,140 +1,121 @@
 package ejercicio3;
 
-public class Matriz1
-{
+public class Matriz1 {
 
-	private int [][] array;
+	private int [][] arr;
 	private static final int NF=10;
-	private static final int NC=7;
+	private static final int NC=10;
 	
-	public Matriz1()
-	{
-		this.array= new int[10][7];
-		cargarPorFilas(this.array);
+//	Constructor vacío
+	public Matriz1() {
+		this.arr= new int[NF][NC];
+		cargarPorFilas(this.arr);
 	}
 	
-	public Matriz1(int nf, int nc)
-	{
-		if(nf>NF)
-		{
-			nf=NF;
+//	Constructor con valores
+	public Matriz1(int nf, int nc) {
+		if(nf > NF) {
+			nf = NF;
 		}
 		
-		if(nc>NC)
-		{
-			nc=NC;
+		if(nc > NC) {
+			nc = NC;
 		}
 		
-		this.array=new int[nf][nc];
-		cargarPorFilas(this.array);
+		this.arr = new int[nf][nc];
+		cargarPorFilas(this.arr);
 	}
 	
-	private static void cargarPorFilas(int[][] array)
-	{
-		int pr=1;
-		for (int i = 0; i < array.length; i++) 
-		{
-			for (int j = 0; j < array[i].length; j++) 
-			{
-				array[i][j]=(int)(pr+Math.random()*10);
+	private static void cargarPorFilas(int[][] arr) {
+		int pr = 1;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j]=(int)(pr + Math.random() * 10);
 			}
-			pr+=10;
+			pr += 10;
 		}
 	}
 	
-	public void ver()
-	{
-		for (int i = 0; i < array.length; i++)
-		{
+	public void ver() {
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println();
-			for (int j = 0; j < array[i].length; j++)
-			{
-				System.out.print(this.array[i][j]+"\t");
+			
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(this.arr[i][j] + "\t");
 			}
+			
 			System.out.println();
 		}
 	}
 	
-	public static void verOtr(int[][] arr)
-	{
-		for (int i = 0; i < arr.length; i++) 
-		{
+	public static void verOtr(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println();
-			for (int j = 0; j < arr[i].length; j++) 
-			{
-				System.out.print(arr[i][j]+"\t");
+			
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
 			}
 			System.out.println();
 		}
 	}
 	
-	public void invertirFilas()
-	{
-		for (int i = 0; i < array.length; i++)
-		{
-			int otr=array[i].length-1;
-			for (int j = 0; j < array[i].length/2; j++) 
-			{
-				int aux=array[i][j];
-				array[i][j]=array[i][otr];
-				array[i][otr]=aux;
+	public void invertirFilas() {
+		for (int i = 0; i < arr.length; i++) {
+			int otr = arr[i].length-1;
+			
+			for (int j = 0; j < arr[i].length / 2; j++) {
+				int aux = arr[i][j];
+				arr[i][j] = arr[i][otr];
+				arr[i][otr] = aux;
 				otr--;
 			}
 		}
 	}
 	
-	public boolean intercambiarFilas(int f1, int f2)//Numero de fila
-	{
-		f1--;//Numero de indice
+	public boolean intercambiarFilas(int f1, int f2) {
+		f1--;
 		f2--;
-		if((f1>array.length)||(f2>array.length))
-		{
+		
+		if((f1 > arr.length) || (f2 > arr.length)) {
 			return false;
 		}
-		int[] aux=array[f1];
-		array[f1]=array[f2];
-		array[f2]=aux;
+		
+		int[] aux = arr[f1];
+		arr[f1] = arr[f2];
+		arr[f2] = aux;
 		return true;
 	}
 	
-	public boolean intercambiarColumnas(int c1, int c2)
-	{
+	public boolean intercambiarColumnas(int c1, int c2) {
 		c1--;
 		c2--;
-		if((c1>array[0].length)||c2>array[0].length)
-		{
+		if((c1 > arr[0].length) || (c2 > arr[0].length)) {
 			return false;
 		}
-		for (int i = 0; i < array.length; i++) 
-		{
-			int aux=array[i][c1];
-			array[i][c1]=array[i][c2];
-			array[i][c2]=aux;
+		for (int i = 0; i < arr.length; i++) {
+			int aux = arr[i][c1];
+			arr[i][c1] = arr[i][c2];
+			arr[i][c2] = aux;
 		}
 		return true;
 	}
 	
-	public int[][] transpuesta()
-	{
-		int[][] transpuesta=new int[this.array[0].length][this.array.length];
-		for (int i = 0; i < array.length; i++) 
-		{
-			for (int j = 0; j < array[i].length; j++) 
-			{
-				transpuesta[j][i]=array[i][j];
+	public int[][] transpuesta() {
+		int[][] transpuesta = new int[this.arr[0].length][this.arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				transpuesta[j][i] = arr[i][j];
 			}
 		}
 		return transpuesta;
 	}
 
-	public void setArrayIJ(int i, int j, int n)
-	{
-		this.array[i][j]=n;
+	public void setArrayIJ(int i, int j, int n) {
+		this.arr[i][j] = n;
 	}
 	
-	public int[][] getArray() 
-	{
-		return array;
+	public int[][] getArray() {
+		return arr;
 		
 	}
 	
